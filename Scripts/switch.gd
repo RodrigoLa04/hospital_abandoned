ee5a7a74c5de57b9d74f4e55265183817c9aaf4b
RodrigoLa04 <rodrigolabrada98@gmail.com>
2025-07-29 15:09:45 -0600

    tutorial-5

--- Scripts/switch.gd
+++ Scripts/switch.gd
@@ -1,11 +1,23 @@
 extends Node3D
 
 @export var on = false
-@export var light_bullb: Node3D
+@export var on_mat: StandardMaterial3D
+@export var off_mat: StandardMaterial3D
+@export var light_ceeling: Node3D
 
-
+func _ready() -> void:
+	if !on:
+		light_ceeling.get_node("light").material_override = off_mat
+	if on:
+		light_ceeling.get_node("light").material_override = on_mat
+	light_ceeling.get_node("OmniLight3D").visible = on
 		
 
 func toggle_light():
-	pass
-	
+	on = !on
+	if on:
+		light_ceeling.get_node("light").material_override = on_mat
+	if !on:
+		light_ceeling.get_node("light").material_override = off_mat
+		
+	light_ceeling.get_node("OmniLight3D").visible = on
