10c3a0af77e1b8fc4416a73c10d293be3f21c3d6
RodrigoLa04 <rodrigolabrada98@gmail.com>
2025-07-30 14:49:11 -0600

    tutorial-6

--- player/interact.gd
+++ player/interact.gd
@@ -1,13 +1,31 @@
 extends RayCast3D
 
+@onready var crosshair = get_parent().get_parent().get_node("Player_ui/CanvasLayer/crosshair")
 
 func _physics_process(delta: float) -> void:
 	if is_colliding():
 		var hit = get_collider()
 		if hit.name == "light_switch":
+			if !crosshair.visible:
+				crosshair.visible = true
 			if Input.is_action_just_pressed("interact"):
 				hit.get_parent().toggle_light()
-		if hit.name == "door":
+		elif hit.name == "door":
+			if !crosshair.visible:
+				crosshair.visible = true
 			#print("esta interactuando")
 			if Input.is_action_just_pressed("interact"):
 				hit.get_parent().get_parent().get_parent().toggle_door()
+				
+		elif hit.name == "drawer":
+			if !crosshair.visible:
+				crosshair.visible = true
+			if Input.is_action_just_pressed("interact"):
+				hit.get_parent().get_parent().toggle_door()
+
+		else: 
+			if crosshair.visible:
+				crosshair.visible = false
+	else: 
+		if crosshair.visible:
+			crosshair.visible = false
--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+const SPEED = 2.5
 const JUMP_VELOCITY = 4.5
 
 func _physics_process(delta: float) -> void:
