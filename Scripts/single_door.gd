023d78723abe4e1d0b2c027fcc5265ecec0272b5
RodrigoLa04 <rodrigolabrada98@gmail.com>
2025-08-03 10:44:28 -0600

    objetos-1

---
+++ Scripts/single_door.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+var opened = false
+
+func toggle_door():
+	if $AnimationPlayer.current_animation != "open":
+		opened = !opened
+		if !opened:
+			$AnimationPlayer.play_backwards("open")
+		if opened:
+			$AnimationPlayer.play("open")
+	
